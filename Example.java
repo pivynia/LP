70ff16fbfd20e6b4959c2aa802bbc52c46b0ec55
pivynia <t5xptpynzv@privaterelay.appleid.com>
2026-04-14 16:44:43 +0700

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,12 @@
+class Example {
+    public static void main(String[] args) {
+        int myVarl; // объявление переменной
+        int myVar2; // объявление еще одной переменной
+        myVarl = 1024; // присваивание переменной myVarl значения 1024
+
+        System.out.println("myVarl содержит " + myVarl);
+        myVar2 = myVarl / 2;
+        System.out.print("myVar2 содержит myVarl / 2: ");
+        System.out.println(myVar2);
+    }
+}
